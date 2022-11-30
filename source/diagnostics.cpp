8ddad99a77fe1c3587c78f9e98a25792dcb449c9
Alexander Lalejini <amlalejini@gmail.com>
2022-11-30 18:31:52 -0500

    add main for diagnostics suite

---
+++ source/diagnostics.cpp
@@ -0,0 +1,35 @@
+// This is the main function for the NATIVE version of this project.
+
+#include <iostream>
+#include <limits>
+
+#include "emp/base/vector.hpp"
+#include "emp/config/command_line.hpp"
+#include "emp/config/ArgManager.hpp"
+
+#include "diagnostics/config.hpp"
+#include "diagnostics/world.hpp"
+#include "diagnostics/org.hpp"
+
+// Hello world
+
+int main(int argc, char* argv[])
+{
+  diag::DiagnosticsConfig config;
+  config.Read("diagnostics.cfg", false);
+  auto args = emp::cl::ArgManager(argc, argv);
+  if (args.ProcessConfigOptions(config, std::cout, "diagnostics.cfg", "diagnostics-macros.h") == false) exit(0);
+  if (args.TestUnknown() == false) exit(0);  // If there are leftover args, throw an error.
+
+  std::cout << "==============================" << std::endl;
+  std::cout << "|    How am I configured?    |" << std::endl;
+  std::cout << "==============================" << std::endl;
+  config.Write(std::cout);
+  std::cout << "==============================\n"
+            << std::endl;
+
+
+  diag::DiagWorld world(config);
+  world.Run();
+
+}
