00dd16cb9acd8cf1a7cbd61d55f7e408cc9fbd85
Alexander Lalejini <amlalejini@gmail.com>
2023-05-07 17:35:38 -0400

    Draft 2023-05-07 diagnostic re-run experiment

---
+++ experiments/2023-05-07-diagnostics/hpc/local-job-gen.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+REPLICATES=10
+EXP_SLUG=2023-05-07-diagnostics
+ACCOUNT=devolab
+SEED_OFFSET=20000
+JOB_TIME=72:00:00
+JOB_MEM=16G
+PROJECT_NAME=phylogeny-informed-evaluation
+
+SCRATCH_EXP_DIR=./test/data/dynamic-ds-lex
+REPO_DIR=/Users/lalejina/devo_ws/phylogeny-informed-evaluation
+HOME_EXP_DIR=${REPO_DIR}/experiments
+
+DATA_DIR=${SCRATCH_EXP_DIR}/${EXP_SLUG}
+JOB_DIR=${SCRATCH_EXP_DIR}/${EXP_SLUG}/jobs
+CONFIG_DIR=${HOME_EXP_DIR}/${EXP_SLUG}/hpc/config
+
+python3 gen-sub.py --time_request ${JOB_TIME} --mem ${JOB_MEM} --data_dir ${DATA_DIR} --config_dir ${CONFIG_DIR} --repo_dir ${REPO_DIR} --replicates ${REPLICATES} --job_dir ${JOB_DIR} --account ${ACCOUNT} --seed_offset ${SEED_OFFSET}
